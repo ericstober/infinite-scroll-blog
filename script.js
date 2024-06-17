73c8462b23f5b3634ce8a2e024d5e052b3e2be48
ericstober <erictstober@gmail.com>
2024-06-17 15:37:37 -0500

    added filter function

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ let page = 1;
 
 // Fetch posts from API
 async function getPosts() {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
 
   const data = await res.json();
 
@@ -24,7 +26,7 @@ async function showPosts() {
     postEl.innerHTML = `
       <div class="number">${post.id}</div>
       <div class="post-info">
-        <h2 class="post-title>${post.title}</h2>
+        <h2 class="post-title">${post.title}</h2>
         <p class="post-body">${post.body}</p>
       </div>
     `;
@@ -33,7 +35,7 @@ async function showPosts() {
   });
 }
 
-// Show loader and fetch more posts
+// Show loader & fetch more posts
 function showLoading() {
   loading.classList.add('show');
 
@@ -44,8 +46,24 @@ function showLoading() {
       page++;
       showPosts();
     }, 300);
-
   }, 1000);
+}
+
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
 }
 
 // Show initial posts
@@ -54,7 +72,9 @@ showPosts();
 window.addEventListener('scroll', () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-  if (scrollTop + clientHeight >= scrollHeight - 5) {
+  if (scrollHeight - scrollTop === clientHeight) {
     showLoading();
   }
 });
+
+filter.addEventListener('input', filterPosts);
